aebb93b33fbf991669036d35bbe8345ee46c71b5
ilja <the.ilja@gmail.com>
2024-02-04 08:22:04 +0000

    publish 0.1.0

--- src/server/app/Http/Controllers/Controller.php
+++ src/server/app/Http/Controllers/Controller.php
@@ -18,9 +18,27 @@ class Controller extends BaseController
      */
     public function welcome (): View
     {
-        $a = Redis::get('aaaa');
+        // $a = Redis::get('aaaa');
+        // Log::channel('socket')->info('test222');
+        $cssFiles = scandir(public_path('static'.DIRECTORY_SEPARATOR.'css'));
+        $cssFiles = array_filter($cssFiles, function (string $file){
+            if ($file == "." || $file == "..") return false;
+            if (pathinfo($file, PATHINFO_EXTENSION) != 'css') return false;
+            if (!str_starts_with($file, 'main')) return false;
+            return true;
+        });
 
-        Log::channel('socket')->info('test222');
-        return view('welcome');
+        $jsFiles = scandir(public_path('static'.DIRECTORY_SEPARATOR.'js'));
+        $jsFiles = array_filter($jsFiles, function (string $file){
+            if ($file == "." || $file == "..") return false;
+            if (pathinfo($file, PATHINFO_EXTENSION) != 'js') return false;
+            // if (!str_starts_with($file, 'main')) return false;
+            return true;
+        });
+
+        return view('welcome', [
+            'cssFiles' => $cssFiles,
+            'jsFiles' => $jsFiles
+        ]);
     }
 }
