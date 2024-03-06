782595f74cb688ed83bfcd040795d1e30af1cef2
ilia <the.ilja@gmail.com>
2024-03-06 06:25:30 +0000

    feedback form

--- src/client/src/widgets/FeedbackModal/FeedbackModal.tsx
+++ src/client/src/widgets/FeedbackModal/FeedbackModal.tsx
@@ -5,6 +5,7 @@ import {Input} from "../../shared/ui/Input/Input";
 import {FormInput} from "../../shared/ui/FormInput/FormInput";
 import {Textarea} from "../../shared/ui/Textarea/Textarea";
 import {ButtonWithLoader} from "../../shared/ui/Button/ButtonWithLoader";
+import {Feedback} from "../../entities/api/Feedback";
 
 type Props = {
   isOpen: boolean
@@ -14,16 +15,39 @@ type Props = {
 
 type State = {
   isLoading: boolean,
+  name: string,
+  email: string,
+  message: string,
 }
 
-export class FeedbackModal extends React.PureComponent<Props, State>
+class FeedbackModal extends React.PureComponent<Props, State>
 {
   public state:State = {
-    isLoading: false
+    isLoading: false,
+    name: "",
+    email: "",
+    message: ""
   }
 
   onSubmit = () => {
     this.setState({isLoading: !this.state.isLoading})
+
+    const data = {
+      name: this.state.name,
+      email: this.state.email,
+      message: this.state.message,
+      variant: this.props.variant
+    }
+
+    Feedback(data, this.onSuccess, this.onError)
+  }
+
+  onSuccess = () => {
+    this.setState({isLoading: false})
+  }
+
+  onError = (error:string) => {
+    alert ('We do not save your feedback, because: ' + error)
   }
 
   render (){
@@ -34,23 +58,29 @@ export class FeedbackModal extends React.PureComponent<Props, State>
       <FormInput title="Please introduce yourself"
                  htmlFor="introduceYourSelf"
                  style={{marginTop: '1rem'}}>
-        <Input value={"asdsad"}
+        <Input value={this.state.name}
                id="introduceYourSelf"
-               disabled={this.state.isLoading}  />
+               maxLength={100}
+               disabled={this.state.isLoading}
+               onChange={(event:React.ChangeEvent<HTMLInputElement>) => this.setState({name: event.target.value})} />
       </FormInput>
       <FormInput title="Leave your contact email"
                  htmlFor="feedbackEmail"
                  style={{marginTop: '.5rem'}}>
-        <Input value={"asdsad"}
+        <Input value={this.state.email}
                id="feedbackEmail"
-               disabled={this.state.isLoading} />
+               maxLength={100}
+               disabled={this.state.isLoading}
+               onChange={(event:React.ChangeEvent<HTMLInputElement>) => this.setState({email: event.target.value})}/>
       </FormInput>
       <FormInput title={(this.props.variant === 'issue') ? 'Please describe the problem in detail' : 'Your message'}
                  htmlFor="feedbackMessage"
                  style={{marginTop: '.5rem'}}>
         <Textarea rows={10}
                   id="feedbackMessage"
-                  disabled={this.state.isLoading}>asdd</Textarea>
+                  maxLength={1024}
+                  disabled={this.state.isLoading}
+                  onChange={(event:React.ChangeEvent<HTMLTextAreaElement>) => this.setState({message: event.target.value})}>{this.state.message}</Textarea>
       </FormInput>
       <div style={{marginTop: '1rem', textAlign: "right", transform: "translateX(-10px)"}}>
         <ButtonWithLoader onClick={this.onSubmit}
@@ -62,3 +92,5 @@ export class FeedbackModal extends React.PureComponent<Props, State>
     </MyModal>
   }
 }
+
+export default FeedbackModal
